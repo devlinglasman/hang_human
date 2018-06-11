901a41a79caf77aeead65050d8fe86f6fce79042
Devlin Glasman <devlin.glasman@gmail.com>
2018-06-11 13:04:48 +0100

    Adds character_present method.

--- game.py
+++ game.py
@@ -1,9 +1,12 @@
 class Game():
 
-    def __init__(self):
-        self
+    def __init__(self, word_to_guess):
+        self.word_to_guess = word_to_guess
 
     def hide_word(self, word):
         hidden_word = len(word) * "_"
         return hidden_word
+
+    def character_present(self, word, char):
+        return char in word
 
--- test_game.py
+++ test_game.py
@@ -5,9 +5,17 @@ from game import *
 class TestGame():
 
     def test_hide_word1(self):
-        game = Game()
+        game = Game("")
         assert game.hide_word("crafter") == "_______"
 
     def test_hide_word2(self):
-        game = Game()
-        assert game.hide_word("devlin") == "______"
+        game = Game("")
+        assert game.hide_word("devlin") == "______" 
+
+    def test_character_present_is_true(self):
+        game = Game("")
+        assert game.character_present("crafter", "a") == True
+
+    def test_character_present_is_false(self):
+        game = Game("")
+        assert game.character_present("crafter", "x") == False
